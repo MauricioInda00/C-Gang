dcc2f1c72e324b9ecde7f0f003e886c95ab42a88
MauricioInda00 <MauricioInda@outlook.es>
2023-07-14 16:37:04 -0300

    Inicio de proyecto de libreria

---
+++ Bookshop/src.c
@@ -0,0 +1,31 @@
+/*
+
+BIENVENIDO A LA LIBRERIA EN C
+
+*/
+
+#include <stdio.h>
+
+// Enum
+
+// Structs
+
+int main()
+{
+    // Variables
+    int opc;
+    // Start
+    puts("Bienvenido a la Tienda de libros en C !\n\n");
+    puts("Ingrese una opcion:\n");
+    scanf("%d", &opc);
+    while (opc != 0)
+    {
+        switch (opc)
+        {
+        case 1:
+            puts("Ingreso un uno:\n");
+            break;
+        }
+    }
+    return 0;
+}
